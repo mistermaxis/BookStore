d8132633303066138c882cd450710cec5603d113
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-09 16:41:41 -0300

    Create folder structure and add dummy components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Books from './components/books.js';
+import Categories from './components/categories.js';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Navbar</h1>
+        <Switch>
+          <Route exact path="/">
+            <Books></Books>
+          </Route>
+          <Route exact path="/categories">
+            <Categories></Categories>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
