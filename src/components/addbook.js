9dc74846c83bcf3b28c462872d2e3df530a47953
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-16 17:56:18 -0300

    Change input value to use state.

--- src/components/addbook.js
+++ src/components/addbook.js
@@ -1,23 +1,36 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { add } from '../redux/books/books.js';
 
 function AddBook() {
+  const [_title, setTitle] = useState('');
+  const [_author, setAuthor] = useState('');
+  const [_id, setId] = useState(0);
   const dispatch = useDispatch();
+
+  const handleChangeTitle = (event) => {
+    setTitle(event.currentTarget.value);
+  };
+
+  const handleChangeAuthor = (event) => {
+    setAuthor(event.currentTarget.value);
+  };
 
   const addBook = (event) => {
     event.preventDefault();
-    const target = event.currentTarget;
     const newBook = {
-      title: target.children[0].value,
-      author: target.children[1].value,
+      title: _title,
+      author: _author,
     };
     dispatch(add(newBook));
+    setTitle('');
+    setAuthor('');
   };
 
   return (
     <form onSubmit={addBook}>
-      <input type="text" required placeholder="Book Title"></input>
-      <input type="text" required placeholder="Book Author"></input>
+      <input onChange={handleChangeTitle} type="text" value={_title} required placeholder="Book Title"></input>
+      <input onChange={handleChangeAuthor} type="text" value={_author} required placeholder="Book Author"></input>
       <button type="submit">Add Book</button>
     </form>
   );
