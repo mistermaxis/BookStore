deda72773cd78e9a4fbe7537301971bc5981598e
Maximiliano <maximiliano_diaz@gmail.com>
2021-11-16 12:08:11 -0300

    Add redux logger as middleware to the store.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,14 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import BooksReducer from './books/books.js';
 
 const reducers = combineReducers({
   reducers: BooksReducer,
 });
 
-const store = createStore(reducers, {});
+const store = createStore(
+  reducers,
+  applyMiddleware(logger)
+);
 
 export default store;
